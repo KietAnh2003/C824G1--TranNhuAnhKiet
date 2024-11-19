c0c233e7718590b5232ca11b95e97e5be1c200d3
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-19 07:49:19 +0700

    up

--- module 3/ss1/bai1.sql
+++ module 3/ss1/bai1.sql
@@ -1,1 +1,13 @@
-SELECT * FROM student_management.class_table;
+
+use student_management;
+create table class (
+    id int auto_increment primary key,  
+    name_class varchar(100) not null        
+);
+-- Tạo bảng Teacher
+create table teacher (
+    id int auto_increment primary key, 
+    name_teacher varchar(100) NOT NULL,  
+    age int,                           
+    country varchar(50)             
+);
