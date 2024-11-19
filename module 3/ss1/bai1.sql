ba1a2a0e6610cdc87521dd1c8df3dd66247354c7
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-19 07:51:06 +0700

    up

--- module 3/ss1/bai1.sql
+++ module 3/ss1/bai1.sql
@@ -1,13 +1,11 @@
-
 use student_management;
 create table class (
-    id int auto_increment primary key,  
-    name_class varchar(100) not null        
+id int auto_increment primary key,  
+name_class varchar(100) not null        
 );
--- Tạo bảng Teacher
 create table teacher (
-    id int auto_increment primary key, 
-    name_teacher varchar(100) NOT NULL,  
-    age int,                           
-    country varchar(50)             
+id int auto_increment primary key, 
+name_teacher varchar(100) NOT NULL,  
+age int,                           
+country varchar(50)             
 );
