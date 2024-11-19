5f4eb20fd8816b78401b7b0b31a19f3e7c9c759b
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-19 16:50:18 +0700

    up

--- module 3/ss2/bai1ss2.sql
+++ module 3/ss2/bai1ss2.sql
@@ -13,16 +13,16 @@ create table phieu_nhap(
     so_phieu_nhap int primary key,
     ngay_nhap date
 );
+create table nha_cung_cap (
+    ma_nha_cung_cap int primary key,
+    ten_nha_cung_cap varchar(100),
+    dia_chi varchar(100)
+);
 create table don_dat_hang(
     so_dat_hang int primary key,
     ngay_dat_hang date,
     ma_nha_cung_cap int,
     foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
-);
-create table nha_cung_cap (
-    ma_nha_cung_cap int primary key,
-    ten_nha_cung_cap varchar(100),
-    dia_chi varchar(100)
 );
 create table phieu_xuat_vat_tu(
     so_phieu_xuat int,
--- module 3/ss2/bai2ss2.sql
+++ module 3/ss2/bai2ss2.sql
@@ -13,16 +13,16 @@ order_total_price double ,
 customer_id int,
 foreign key (customer_id) references customers(customer_id)
 );
+create table products (
+product_id int primary key,
+product_name varchar(100),
+product_price double
+);
 create table order_detail(
 order_id int ,
 product_id int,
  order_detail_qty varchar(100),
  foreign key (order_id) references orders(order_id),
  foreign key (product_id) references products(product_id)
-);
-create table products (
-product_id int primary key,
-product_name varchar(100),
-product_price double
 );
 
