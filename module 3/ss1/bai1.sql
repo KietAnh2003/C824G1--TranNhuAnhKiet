005b334b6f1cbcedaa0ae867520b7656a695382f
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-19 08:51:47 +0700

    up

--- module 3/ss1/bai1.sql
+++ module 3/ss1/bai1.sql
@@ -1,11 +1,11 @@
 use student_management;
-create table class (
+create table classes (
 id int auto_increment primary key,  
 name_class varchar(100) not null        
 );
-create table teacher (
+create table teachers (
 id int auto_increment primary key, 
-name_teacher varchar(100) NOT NULL,  
-age int,                           
+name_teacher varchar(100) not null,  
+age int not null,                           
 country varchar(50)             
 );
