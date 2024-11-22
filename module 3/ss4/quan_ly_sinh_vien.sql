811bb318dbcf75e1637b9921c0f7f752aa4b554f
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-22 08:35:44 +0700

    up

--- module 3/ss4/quan_ly_sinh_vien.sql
+++ module 3/ss4/quan_ly_sinh_vien.sql
@@ -1,39 +1,39 @@
-create database quan_ly_sinh_vien;
-use quan_ly_sinh_vien;
+create database manager_students;
+use manager_students;
 
 create table students (
     id int auto_increment primary key,
-    ho_ten varchar(100),
-    ngay_sinh date,
-    dia_chi varchar(255)
+    name_student  varchar(100),
+    date_of_birth date,
+    address varchar(255)
 );
 create table subjects (
     id int auto_increment primary key,
-    ten_mon_hoc varchar(100),
+    name_subject varchar(100),
     credit int
 );
 create table marks (
     id int auto_increment primary key,
     student_id int,
     subject_id int,
-    diem decimal(5,2),
+    mark decimal(5,2),
     foreign key (student_id) references students(id),
     foreign key (subject_id) references subjects(id)
 );
 
-insert into students (ho_ten, ngay_sinh, dia_chi) values
+insert into students (name_student, date_of_birth, address) values
 ('nguyễn văn a', '2000-01-01', 'hà nội'),
 ('trần thị b', '1999-05-22', 'đà nẵng'),
 ('phạm minh c', '2001-07-10', 'tp hồ chí minh'),
 ('lê thị d', '1998-03-15', 'quảng trị'),
 ('hoàng văn e', '1997-09-09', 'nha trang');
-insert into subjects (ten_mon_hoc, credit) values
+insert into subjects (name_subject, credit) values
 ('toán', 3),
 ('lý', 4),
 ('hóa', 3),
 ('tin học', 2),
 ('anh văn', 2);
-insert into marks (student_id, subject_id, diem) values
+insert into marks (student_id, subject_id, mark) values
 (1, 1, 8.5),
 (1, 2, 9.0),
 (2, 3, 7.5),
@@ -47,14 +47,14 @@ insert into marks (student_id, subject_id, diem) values
 
 select * from subjects where credit = (select max(credit) from subjects);
 
-select subjects.id, subjects.ten_mon_hoc, subjects.credit, marks.diem 
+select subjects.id, subjects.name_subject, subjects.credit, marks.mark 
 from subjects 
 join marks on subjects.id = marks.subject_id 
-where marks.diem = (select max(diem) from marks);
+where marks.mark = (select max(mark) from marks);
 
-select students.id, students.ho_ten, avg(marks.diem) as diem_trung_binh 
+select students.id, students.name_student, avg(marks.mark) as average_mark 
 from students 
 join marks on students.id = marks.student_id 
-group by students.id, students.ho_ten 
-order by diem_trung_binh desc;
+group by students.id, students.name_student
+order by average_mark  desc;
 
