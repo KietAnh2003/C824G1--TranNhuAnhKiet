09cb6d6f5318b111ddc5b4a24f8b12df8cc88320
KietAnh2003 <kietanhvip12@gmail.com>
2024-11-28 16:21:12 +0700

    up

---
+++ module 3/ss9/product_discount_calculator/src/main/java/com/example/product_discount_calculator/DiscountServlet.java
@@ -0,0 +1,28 @@
+package com.example.product_discount_calculator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "DiscountServlet", urlPatterns = "/display-discount")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription = request.getParameter("productDescription");
+        double listPrice = Double.parseDouble(request.getParameter("listPrice"));
+        double discountPercent = Double.parseDouble(request.getParameter("discountPercent"));
+
+        double discountAmount = listPrice * discountPercent * 0.01;
+        double discountPrice = listPrice - discountAmount;
+
+        request.setAttribute("productDescription", productDescription);
+        request.setAttribute("listPrice", listPrice);
+        request.setAttribute("discountPercent", discountPercent);
+        request.setAttribute("discountAmount", discountAmount);
+        request.setAttribute("discountPrice", discountPrice);
+
+        request.getRequestDispatcher("result.jsp").forward(request, response);
+    }
+}
---
+++ module 3/ss9/product_discount_calculator/src/main/webapp/result.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dispay discount</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Dispay discount <Discount></Discount></h1>
+<form style="text-align: left">
+<p>Product Description: ${productDescription}</p>
+<p>List Price: $${listPrice}</p>
+<p>Discount Percent: ${discountPercent}%</p>
+<p>Discount Amount: $${discountAmount}</p>
+<p>Discount Price: $${discountPrice}</p>
+</form>
+</body>
+</html>
